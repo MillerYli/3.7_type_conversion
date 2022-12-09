e310d522fbe77df13b0dcb1e72c4c71b58f0cf6e
Yulia Miller <miller82.98@mail.ru>
2022-12-08 22:09:07 -0800

    Data of birth

--- 3.7_type_conversion/Program.cs
+++ 3.7_type_conversion/Program.cs
@@ -7,17 +7,21 @@ namespace MyNameSpase
         public static void Main(string[] args)
         {
             Console.Write("Enter your name: ");     
-            string name = Console.ReadLine();       //Ввести свое имя
+            var name = Console.ReadLine();       //Ввести свое имя
 
             Console.Write("Enter your age: ");
-            int age= checked ((byte) int.Parse(Console.ReadLine()));        //Ввести свой возраст
+            var age= Console.ReadLine();        //Ввести свой возраст
+
+            Console.Write("Enter your date of birth: ");
+            var birthday = Console.ReadLine();
 
             Console.Write("What is your favorite day of week? ");
             DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());       //Ввести  свой любимый день недели
 
             Console.WriteLine("Your name is {0} and your is {1}", name, age);
-            
-            Console.WriteLine("Your favorite day is {0}", day);
+            Console.WriteLine("Your birthday: {0}", birthday);
+            Console.WriteLine("And your favorite day is {0}", day);
+            Console.WriteLine("Nice to meet you and have a nice day :)");
         }
     }
 }
