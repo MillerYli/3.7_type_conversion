b30d486733eea353908e263a14e9d787a0a602af
Yulia Miller <miller82.98@mail.ru>
2022-12-08 21:45:51 -0800

    Добавьте файлы проекта.

---
+++ 3.7_type_conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyNameSpase
+{
+    class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");     
+            string name = Console.ReadLine();       //Ввести свое имя
+
+            Console.Write("Enter your age: ");
+            int age= checked ((byte) int.Parse(Console.ReadLine()));        //Ввести свой возраст
+
+            Console.Write("What is your favorite day of week? ");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());       //Ввести  свой любимый день недели
+
+            Console.WriteLine("Your name is {0} and your is {1}", name, age);
+            
+            Console.WriteLine("Your favorite day is {0}", day);
+        }
+    }
+}
